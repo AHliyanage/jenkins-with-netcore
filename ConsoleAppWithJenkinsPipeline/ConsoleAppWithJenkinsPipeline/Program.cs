1c04e305c7ea18ec8ec056ebbfab545506e527dc
Arosha Liyanage <arosha.l@fidenz.com>
2025-09-06 17:58:49 +0530

    Add new console proj.

---
+++ ConsoleAppWithJenkinsPipeline/ConsoleAppWithJenkinsPipeline/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Program.Run();
+
+public partial class Program
+{
+    public static void Run()
+    {
+        Console.WriteLine("Hello, First testing Jenkins with .Net console!");
+
+        var x = 10;
+        var y = 20;
+        var sum = MathHelper.Add(x, y);
+        Console.WriteLine($"The sume is: {sum}");
+    }
+}
+
+public static class MathHelper
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ ConsoleAppWithJenkinsPipeline/TestProject1/Test1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class MathHelperTests
+    {
+        [TestMethod]
+        public void Add_ReturnsCorrectSum_ForPositiveNumbers()
+        {
+            Assert.AreEqual(30, MathHelper.Add(10, 20));
+        }
+
+        [TestMethod]
+        public void Add_ReturnsCorrectSum_ForNegativeNumbers()
+        {
+            Assert.AreEqual(-12, MathHelper.Add(-5, -7));
+        }
+
+        [TestMethod]
+        public void Add_ReturnsCorrectSum_ForZero()
+        {
+            Assert.AreEqual(0, MathHelper.Add(0, 0));
+        }
+
+        [TestMethod]
+        public void Run_PrintsHelloMessageFirst()
+        {
+            // Arrange
+            using var sw = new StringWriter();
+            Console.SetOut(sw);
+
+            // Act
+            Program.Run();
+
+            // Assert
+            var output = sw.ToString();
+            var firstLine = output.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)[0];
+            Assert.AreEqual("Hello, First testing Jenkins with .Net console!", firstLine);
+        }
+    }
+}
